d6c2256e0aee473a454e72d20e5e992cc22e2f90
David A Pears <davidapears@gmail.com>
2018-11-26 18:06:25 +0000

    Changes default zoom and lat/long on gmaps

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,15 +4,15 @@ class Map extends Component {
    render() {
    const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
-        defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
-        defaultZoom = { 13 }
+        defaultCenter = { { lat: 51.5074, lng: -0.1278 } }
+        defaultZoom = { 4.5 }
       >
       </GoogleMap>
    ));
    return(
       <div className="google-map">
         <GoogleMapExample
-          containerElement={ <div style={{ height: `1000px`, width: '750px' }} /> }
+          containerElement={ <div style={{ height: `750px`, width: '750px' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
       </div>
