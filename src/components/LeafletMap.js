047371925650196b3c7c90319802d9b4dde0b665
David A Pears <davidapears@gmail.com>
2018-12-05 14:50:29 +0000

    Links inside pin-pop-ups now work and open new tab with travel article

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -19,9 +19,10 @@ class LeafletMap extends React.Component {
 
         var map = L.map(element);
 
-        // Marker1 = LONDON, ENGLAND
-        var marker1 = L.marker([51.515968, -0.174970]).addTo(map);
-        marker1.bindPopup("<b>London</b><br>Article Title<br>Publication Name<br>Link").openPopup();
+        // Marker1 = Schengen, Luxembourg
+        var marker1 = L.marker([49.4709, 6.3650]).addTo(map);
+        marker1.bindPopup(
+          "<b>Schengen, Luxembourg</b><br>'Schengen: A tiny village that changed European travel`<br><b>BBC Travel</b><br><a href=http://www.bbc.com/travel/story/20181202-schengen-a-tiny-village-that-changed-european-travel>click to read").openPopup();
 
         // Marker2 = AMSTERDAM, NETHERLANDS
         var marker2 = L.marker([52.3680, 4.9036]).addTo(map);
