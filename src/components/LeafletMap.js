2e3db9d747e9119600fb1b25085f3bd6840df4b1
David A Pears <davidapears@gmail.com>
2018-12-05 14:15:46 +0000

    Adds pins and pop-ups to Leaflet map, in order to link to travel articles from around the world

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -9,16 +9,34 @@ class LeafletMap extends React.Component {
         };
     }
 
+    setupMap(){
+        this.map.setView([51.515968, -0.174970], 4,);
+        // NB: Lat/Long hardcoded to Paddington, London, Writers base/office
+    }
+
+
     createMap(element){
+
         var map = L.map(element);
+
+        var marker = L.marker([51.515968, -0.174970]).addTo(map);
+        // Marker1 = LONDON, ENGLAND
+        var marker = L.marker([52.3680, 4.9036]).addTo(map);
+        // Marker2 = AMSTERDAM, NETHERLANDS
+        var marker = L.marker([23.1136, -82.3666]).addTo(map);
+        // Marker3 = HAVANA, CUBA
+        var marker = L.marker([6.9271, 79.8612]).addTo(map);
+        // Marker4 = COLOMBO, SRI LANKA
+
+        var popup = L.popup()
+          .setLatLng([55.9533, -3.1883])
+          .setContent("Edinburgh (Stand-alone pop-up)")
+          .openOn(map);
+
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
         }).addTo(map);
         return map;
-    }
-
-    setupMap(){
-        this.map.setView([51.508, -0.11], 500,);
     }
 
     componentDidMount(){
@@ -35,9 +53,11 @@ class LeafletMap extends React.Component {
     render(){
 
       return(
-         <div className="map">
-
+        <div>
+         <div className="leafletmap">
+         // NB: Map renders in this div!
          </div>
+        </div>
       )
         /*Returns div with id map*/
     }
